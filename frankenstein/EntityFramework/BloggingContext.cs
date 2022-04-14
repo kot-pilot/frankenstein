4462ec995147af0a92913883af40f73241fd5b50
Yauheni Lavitski <ylavitski@artezio.com>
2022-04-14 21:47:20 +0300

    first migration commit

---
+++ frankenstein/EntityFramework/BloggingContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework
+{
+    
+
+    public class BloggingContext : DbContext
+    {
+
+        public BloggingContext()
+        {
+        }
+        public DbSet<First> First { get; set; }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost;Database=frankenstein;Trusted_Connection=True;");
+    }
+
+    public class First
+    {
+        public int Id { get; set; }
+    }
+
+}
