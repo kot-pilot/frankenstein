7b486260016063e082f1fe88e748fda74f2cdde2
Yauheni Lavitski <ylavitski@artezio.com>
2022-04-14 21:54:41 +0300

    second migration

--- frankenstein/EntityFramework/BloggingContext.cs
+++ frankenstein/EntityFramework/BloggingContext.cs
@@ -12,13 +12,22 @@ namespace EntityFramework
         }
         public DbSet<First> First { get; set; }
 
+        public DbSet<Second> Second { get; set; }
+
         // The following configures EF to create a Sqlite database file in the
         // special "local" folder for your platform.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlServer("Server=localhost;Database=frankenstein;Trusted_Connection=True;");
     }
 
+   
+
     public class First
+    {
+        public int Id { get; set; }
+    }
+
+    public class Second
     {
         public int Id { get; set; }
     }
